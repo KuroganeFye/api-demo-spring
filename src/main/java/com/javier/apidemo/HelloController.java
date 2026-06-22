f7d2a06fd393b3d1976c84596ccc024f903ee2f2
Javier Sánchez <javiersanchezmartinez@gmail.com>
2026-06-22 14:03:45 -0600

    Add POST endpoint and JSON response

--- src/main/java/com/javier/apidemo/HelloController.java
+++ src/main/java/com/javier/apidemo/HelloController.java
@@ -1,15 +1,35 @@
 package com.javier.apidemo;
 
+import java.util.HashMap;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController 
 {
 
     @GetMapping("/hello")
-    public String hello() 
+    public Map<String, String> hello() 
     {
-        return "Hola Javier, tu API ya funciona 🚀";
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "API funcionando");
+        response.put("user", "Javier");
+        return response;
+    }
+
+    @PostMapping("/users")
+    public Map<String, String> createUser(@RequestBody Map<String, String> body) 
+    {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Usuario creado");
+        response.put("name", body.get("name"));
+        return response;
     }
 }
