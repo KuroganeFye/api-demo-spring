995b14d5956d42d8727f6299b5a97fd9f12043d3
KuroganeFye <javiersanchezmartinez@gmail.com>
2026-07-22 14:39:36 -0600

    Añadido un nuevo archivo UserServiceImpl.java que implementa la interfaz UserService. Este archivo contiene la lógica de negocio para manejar las operaciones relacionadas con los usuarios, como agregar, eliminar y obtener usuarios. Además, se ha modificado el archivo UserService.java para reflejar los cambios necesarios en la implementación del servicio de usuario.

--- src/main/java/com/javier/apidemo/UserService.java
+++ src/main/java/com/javier/apidemo/UserService.java
@@ -1,50 +1,10 @@
 package com.javier.apidemo;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService 
+public interface UserService 
 {
-
-    private List<User> users = new ArrayList<>();
-    private int nextId = 1;
-
-    public List<User> getUsers() 
-    {
-        return users;
-    }
-
-    public User createUser(User user) 
-    {
-        
-        if (user.getAge() < 0)
-        {
-            throw new InvalidUserException("Age cannot be negative");
-        }
-        else if (user.getName() == null || user.getName().isEmpty())
-        {
-            throw new InvalidUserException("Name cannot be empty");
-        }
-
-        user.setId(nextId);
-        nextId++;
-        users.add(user);
-        return user;
-    }
-
-    public User getUserById(Integer id)
-    {
-        for (User user : users)
-        {
-            if (user.getId().equals(id))
-            {
-                return user;
-            }
-        }
-
-        throw new UserNotFoundException(id);
-    }
+    List<User> getUsers();
+    User getUserById(Integer id);
+    User createUser(User user);
 }
